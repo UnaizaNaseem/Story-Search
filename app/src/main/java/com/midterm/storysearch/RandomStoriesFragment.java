818ee6adeeaecae5dd2f091586c8e9f3d06b20e2
unaizanaseem78@gmail.com <unaizanaseem78@gmail.com>
2023-12-03 21:53:03 +0500

    fixed bugs in random stories

--- app/src/main/java/com/midterm/storysearch/RandomStoriesFragment.java
+++ app/src/main/java/com/midterm/storysearch/RandomStoriesFragment.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -30,19 +31,40 @@ public class RandomStoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_random_stories, container, false);
 
+        if (!tryOpenCorpus(view.getContext())) {
+            return view;
+        }
+
         getRandomDocIds(view.getContext());
         Button randomizeBtn = view.findViewById(R.id.btnRandom);
 
         randomizeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onRandomizeButtonClick(v); // Remove the argument 'view'
+                onRandomizeButtonClick(v);
             }
         });
 
         displayRandomStories(view);
 
         return view;
+    }
+
+    private boolean tryOpenCorpus(Context context) {
+        String dbPath = context.getFilesDir() + "/corpus.db";
+
+        File dbFile = new File(dbPath);
+        if (!dbFile.exists()) {
+            return false;
+        }
+
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READONLY);
+        if (db == null) {
+            return false;
+        }
+
+        db.close();
+        return true;
     }
 
 
